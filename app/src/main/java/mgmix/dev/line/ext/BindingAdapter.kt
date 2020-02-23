076b780734ddecd3f4689c3b3b91d92f10b12fb3
Evan Song <evace93@gmail.com>
2020-02-23 17:57:58 +0900

    Image bindingAdapter 추가

--- app/src/main/java/mgmix/dev/line/ext/BindingAdapter.kt
+++ app/src/main/java/mgmix/dev/line/ext/BindingAdapter.kt
@@ -1,13 +1,21 @@
 package mgmix.dev.line.ext
 
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
-@BindingAdapter("setImage")
+@BindingAdapter("app:setImage")
 fun setImage(view: ImageView, url: String?) {
     // TODO if null delete imageView
     Glide.with(view.context)
         .load(url)
         .into(view)
+}
+
+@BindingAdapter("app:setVisible")
+fun setVisible(view: View, visible: Boolean) {
+    Log.e("Binding", "visible: $visible ")
+    view.visibility = if (visible) View.VISIBLE else View.GONE
 }
